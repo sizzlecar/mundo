66e2cdcf485df8e9bab273ba7692376ee98c0520
sizzlecarl <763667966@qq.com>
2022-01-31 03:10:25 +0800

    add unit test

---
+++ mundo-core/src/test/java/com/bluslee/mundo/core/test/process/base/BaseElementTest.java
@@ -0,0 +1,25 @@
+package com.bluslee.mundo.core.test.process.base;
+
+import com.bluslee.mundo.core.process.base.BaseElement;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * BaseElementTest.
+ *
+ * @author carl.che
+ */
+public class BaseElementTest {
+
+    @Test
+    public void equalsTest() {
+        BaseElement baseElement1 = new BaseElement("el1", "el1") {
+        };
+        BaseElement baseElement2 = new BaseElement("el1", "el2") {
+        };
+        BaseElement baseElement3 = new BaseElement("el2", "el2") {
+        };
+        Assert.assertEquals(baseElement1, baseElement2);
+        Assert.assertNotEquals(baseElement1, baseElement3);
+    }
+}
